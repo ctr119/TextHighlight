710aedc0142bdf9c09658d873e99f859daa361e7
Victor Barrios <victor.barrios@bestsecret.com>
2024-04-05 23:28:25 +0200

    Initial Commit

---
+++ TextHighlightAnimation/TextHighlightAnimationApp.swift
@@ -0,0 +1,17 @@
+//
+//  TextHighlightAnimationApp.swift
+//  TextHighlightAnimation
+//
+//  Created by Barrios, Victor on 5/4/24.
+//
+
+import SwiftUI
+
+@main
+struct TextHighlightAnimationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
