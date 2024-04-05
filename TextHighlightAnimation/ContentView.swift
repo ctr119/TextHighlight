272ef67eada3401222e33ee78ed6d9318117b838
Victor Barrios <victor.barrios@bestsecret.com>
2024-04-06 00:22:55 +0200

    Add a basic text that gets hightligthed horizontally

--- TextHighlightAnimation/ContentView.swift
+++ TextHighlightAnimation/ContentView.swift
@@ -1,21 +1,53 @@
-//
-//  ContentView.swift
-//  TextHighlightAnimation
-//
-//  Created by Barrios, Victor on 5/4/24.
-//
-
 import SwiftUI
 
 struct ContentView: View {
+    @State private var width: CGFloat = 0
+    private let targetWidth: CGFloat = 50
+    
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+            Spacer()
+            
+            Text(
+"""
+Lorem ipsum dolor sit amet
+"""
+            )
+                .padding(2)
+                .background(
+                    GeometryReader {
+                        geometry in
+                        HStack {
+                            RoundedRectangle(cornerRadius: 5)
+                                .fill(Color.yellow)
+                                .frame(
+                                    width: width <= geometry.size.width ? width : geometry.size.width,
+                                    height: geometry.size.height
+                                )
+                            Spacer()
+                        }
+                    }
+                )
+            
+            Spacer()
+            buttons
         }
-        .padding()
+    }
+    
+    private var buttons: some View {
+        HStack {
+            Button("Start") {
+                withAnimation {
+                    width += targetWidth
+                }
+            }
+            
+            Button("Reset") {
+                withAnimation {
+                    width = 0
+                }
+            }
+        }
     }
 }
 
